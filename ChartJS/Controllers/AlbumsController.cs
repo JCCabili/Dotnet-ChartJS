95654185cdc1f8bf9f4cdee33d9bb405c48d78bf
JC <cabilijc@gmail.com>
2018-09-29 04:18:39 +0800

    Initial src upload

---
+++ ChartJS/Controllers/AlbumsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ChartJS.Models;
+
+namespace ChartJS.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        private ChartJSContext db = new ChartJSContext();
+
+        // GET: Albums
+        public ActionResult Index()
+        {
+            return View(db.Albums.ToList());
+        }
+
+        // GET: Albums/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Albums albums = db.Albums.Find(id);
+            if (albums == null)
+            {
+                return HttpNotFound();
+            }
+            return View(albums);
+        }
+
+        // GET: Albums/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Albums/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Price,SoldDate")] Albums albums)
+        {
+
+            //albums.SoldDate = DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                db.Albums.Add(albums);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(albums);
+        }
+
+        // GET: Albums/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Albums albums = db.Albums.Find(id);
+            if (albums == null)
+            {
+                return HttpNotFound();
+            }
+            return View(albums);
+        }
+
+        // POST: Albums/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Price,SoldDate")] Albums albums)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(albums).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(albums);
+        }
+
+        // GET: Albums/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Albums albums = db.Albums.Find(id);
+            if (albums == null)
+            {
+                return HttpNotFound();
+            }
+            return View(albums);
+        }
+
+        // POST: Albums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Albums albums = db.Albums.Find(id);
+            db.Albums.Remove(albums);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        public ActionResult DashBoard()
+        {
+
+            var allsales = db.Albums.ToList();
+
+
+            List<DateTime> dates = allsales.Select(r => r.SoldDate.Date).OrderByDescending(a => a.Date).Distinct().ToList();
+            List<decimal> sales = new List<decimal>();
+
+
+            List<string> dateString = new List<string>();       
+            foreach (var item in dates)
+            {
+                sales.Add(allsales.Where(x => x.SoldDate.Date >= item.Date && 
+                x.SoldDate.Date <= item.Date)
+                    .Sum(a=>a.Price));
+
+                dateString.Add(item.ToString("MM/dd/yyyy"));
+            }
+
+            ViewBag.dateList = dateString;
+            ViewBag.salesList = sales;
+
+            return View();
+        }
+    }
+}
